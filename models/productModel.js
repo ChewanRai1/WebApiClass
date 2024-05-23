d7add9237428d9589f9a67692971ca8a3d2170d0
ChewanRai1 <220154@softwarica.edu.np>
2024-05-23 23:28:46 +0545

    image uploaded succesfully

--- models/productModel.js
+++ models/productModel.js
@@ -1,32 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    productName : {
-        type: String,
-        required :true,
-    },
-    productPrice : {
-        type : Number,
-        required : true,
-    },
-    productDescription : {
-        type: String,
-        required :true,
-        maxlength : 300,
-    },
-    productCategory : {
-        type : String,
-        required : true,
-    },
-    productImage:{
-        type: String,
-        required: true
-    },
-    createAt : {
-        type : Date.now,
-    }
-})
-const porduct = mongoose.model('products', productSchema)
+  productName: {
+    type: String,
+    required: true,
+  },
+  productPrice: {
+    type: Number,
+    required: true,
+  },
+  productDescription: {
+    type: String,
+    required: true,
+    maxlength: 300,
+  },
+  productCategory: {
+    type: String,
+    required: true,
+  },
+  productImage: {
+    type: String,
+    required: true,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+const Products = mongoose.model("products", productSchema);
 module.exports = Products;
 
 //
